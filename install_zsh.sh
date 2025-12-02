65f982bccfb6756d9d6a8ac51311f89e4af44b9a
miomelliot <s89652158910@gmail.com>
2025-12-02 18:11:43 +0300

    fedora add

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 set -e
 
-# Проверяем, что это Arch Linux
-if [ ! -f /etc/arch-release ]; then
-    echo "Скрипт работает только на Arch Linux. Выходим."
+# Проверяем, что это Fedora Linux
+if [ ! -f /etc/fedora-release ]; then
+    echo "Скрипт работает только на Fedora Linux. Выходим."
     exit 1
 fi
 
 echo "Обновляем систему и устанавливаем необходимые пакеты..."
-sudo pacman -Syu --noconfirm zsh git zoxide eza neovim fzf curl fd bat lazygit
+sudo dnf upgrade --refresh -y
+sudo dnf install -y zsh git zoxide neovim fzf curl fd-find bat dnf-plugins-core
+
+echo "Подключаем Copr-репозитории для eza и lazygit..."
+sudo dnf copr enable -y alternateved/eza
+sudo dnf copr enable -y atim/lazygit
+
+echo "Устанавливаем eza и lazygit..."
+sudo dnf install -y eza lazygit
 
 # Определяем пути для oh-my-zsh и кастомных файлов
 export ZSH="$HOME/.oh-my-zsh"
@@ -76,90 +84,27 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
-
 # Path to your Oh My Zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time Oh My Zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-# Игнорировать команды, начинающиеся с пробела
+# История
 setopt HIST_IGNORE_SPACE
-
-# Немедленно добавлять команды в историю
 setopt INC_APPEND_HISTORY
-
-# Разделять историю между всеми сессиями
 setopt SHARE_HISTORY
-
-# Игнорировать последовательные дубликаты команд
 setopt HIST_IGNORE_DUPS
 
-# Set list of themes to pick from when loading at random
-# Setting this variable when ZSH_THEME=random will cause zsh to load
-# a theme from this variable instead of looking in $ZSH/themes/
-# If set to an empty array, this variable will have no effect.
-# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
-
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
-
-# Uncomment the following line to use hyphen-insensitive completion.
-# Case-sensitive completion must be off. _ and - will be interchangeable.
 HYPHEN_INSENSITIVE="true"
 
-# Uncomment one of the following lines to change the auto-update behavior
-# zstyle ':omz:update' mode disabled  # disable automatic updates
-zstyle ':omz:update' mode auto      # update automatically without asking
-# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
-
-# Uncomment the following line to change how often to auto-update (in days).
+zstyle ':omz:update' mode auto
 zstyle ':omz:update' frequency 14
 
-# Uncomment the following line if pasting URLs and other text is messed up.
-# DISABLE_MAGIC_FUNCTIONS="true"
-
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
-
-# Uncomment the following line to disable auto-setting terminal title.
 DISABLE_AUTO_TITLE="true"
-
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
-
-# Uncomment the following line to display red dots whilst waiting for completion.
-# You can also set it to another string to have that shown instead of the default red dots.
-# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
-# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
 COMPLETION_WAITING_DOTS="true"
 
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# You can set one of the optional three formats:
-# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
 HIST_STAMPS="yyyy-mm-dd"
 
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 plugins=(
   git
   zsh-autosuggestions
@@ -178,38 +123,9 @@ source $ZSH/oh-my-zsh.sh
 # Инициализация zoxide (должно быть после Oh My Zsh)
 eval "$(zoxide init --cmd cd zsh)"
 
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='nvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch $(uname -m)"
-
-# Set personal aliases, overriding those provided by Oh My Zsh libs,
-# plugins, and themes. Aliases can be placed here, though Oh My Zsh
-# users are encouraged to define aliases within a top-level file in
-# the $ZSH_CUSTOM folder, with .zsh extension. Examples:
-# - $ZSH_CUSTOM/aliases.zsh
-# - $ZSH_CUSTOM/macos.zsh
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-
+# Алиасы
 alias lsp="eza --tree --level=1 --icons=always -l --octal-permissions"
 alias ls="eza --tree --level=1 --icons=always --no-time --no-user --no-permissions"
-alias fd='fdfind'
 
 # Если в системе нет batcat (Debian-нейминг), используем bat
 if ! command -v batcat >/dev/null 2>&1 && command -v bat >/dev/null 2>&1; then
@@ -253,9 +169,6 @@ dumpctx() {
 
   rm -f "$tmp_output"
 }
-# ctx
-# ctx -E Dockerfile
-# ctx -d2
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
@@ -266,7 +179,7 @@ pathadd() {
 pathadd "$HOME/.local/bin"
 pathadd "/usr/local/nvim/bin"
 
-# Снипиты
+# Сниппеты / утилиты
 alias ctx='dumpctx'
 alias vsc='code . --reuse-window'
 alias cat="bat --paging=never" #  --style=plain
